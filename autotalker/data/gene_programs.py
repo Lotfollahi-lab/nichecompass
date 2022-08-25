d237fe473d967e62b2a20bbfe32024e013aedfee
sebastianbirk <sebastian.birk@outlook.com>
2022-08-25 14:00:06 +0200

    Update gene program scrit

--- autotalker/data/gene_programs.py
+++ autotalker/data/gene_programs.py
@@ -19,14 +19,25 @@ def download_nichenet_ligand_target_mx(
         df_save_path=save_path)
 
 
-def extract_mask_from_ligand_target_mx(path: str, n_genes_per_ligand: int=10):
+def extract_gene_programs_from_ligand_target_mx(path: str,
+                                                keep_target_ratio: float=0.1):
     """
     
     """
-    df = pd.read_csv(path)
-    idx_targets_sorted = np.argsort(-df.values, axis=1)[:, :n_genes_per_ligand]
-    df_sorted = pd.DataFrame(df.columns.values[idx_targets_sorted],
-                             index=df.index + "_ligand")
+    ligand_target_df = pd.read_csv(path, index_col=0)
+    all_target_gene_scores = np.squeeze(ligand_target_df.values).flatten()
+    all_target_gene_scores.sort()
+    all_target_gene_scores_sorted = np.flip(all_target_gene_scores)
+    score_keep_threshold = all_target_gene_scores_sorted[int(
+        len(all_target_gene_scores_sorted) * keep_target_ratio)]
+    ligand_target_df = ligand_target_df.applymap(
+        lambda x: x > score_keep_threshold)
+    ligand_target_dict = ligand_target_df.to_dict()
+    for ligand in ligand_target_dict.keys():
+        ligand_target_dict[ligand] = [target for target, bool_value in 
+                                      ligand_target_dict[ligand].items() if 
+                                      bool_value == True]
+    return ligand_target_dict
 
 
 
@@ -34,56 +45,22 @@ def extract_mask_from_ligand_target_mx(path: str, n_genes_per_ligand: int=10):
 
 # add binary I of size n_vars x number of annotated terms in files
 # if I[i,j]=1 then gene i is active in annotation j
-def add_gp_annotations(adata: ad.AnnData,
-                       file_name: str,
-                       min_genes=0,
-                       max_genes=None,
-                       varm_key="I",
-                       uns_key="terms",
-                       clean=True,
-                       genes_use_upper=True):
+def add_gene_program_node_mask(adata: ad.AnnData,
+                                           file_name: str,
+                                           min_genes=0,
+                                           max_genes=None,
+                                           varm_key="I",
+                                           uns_key="gene_programs",
+                                           clean=True,
+                                           genes_uppercase=True):
     """
-    Add gene program annotations stored in a csv file to an AnnData object.
-    Inspired by https://github.com/theislab/scarches.
 
-    Parameters
-    ----------
-    adata
-        Annotated data matrix.
-    files
-        Paths to text files with annotations. The function considers rows to be gene sets
-        with name of a gene set in the first column followed by names of genes.
-    min_genes
-        Only include gene sets which have the total number of genes in adata
-        greater than this value.
-    max_genes
-        Only include gene sets which have the total number of genes in adata
-        less than this value.
-    varm_key
-        Store the binary array I of size n_vars x number of annotated terms in files
-        in `adata.varm[varm_key]`. if I[i,j]=1 then the gene i is present in the annotation j.
-    uns_key
-        Store gene sets' names in `adata.uns[uns_key]`.
-    clean
-        If 'True', removes the word before the first underscore for each term name (like 'REACTOME_')
-        and cuts the name to the first thirty symbols.
-    genes_use_upper
-        if 'True', converts genes' names from files and adata to uppercase for comparison.
     """
-    files = [files] if isinstance(files, str) else files
-    annot = []
+    gp_dict = extract_gene_programs_from_ligand_target_mx(file_name)s
 
-    for file in files:
-        with open(file) as f:
-            p_f = [l.upper() for l in f] if genes_use_upper else f
-            terms = [l.strip('\n').split() for l in p_f]
-
-        if clean:
-            terms = [[term[0].split('_', 1)[-1][:30]]+term[1:] for term in terms if term]
-        annot+=terms
-
-    var_names = adata.var_names.str.upper() if genes_use_upper else adata.var_names
+    var_names = adata.var_names.str.upper() if genes_uppercase else adata.var_names
     I = [[int(gene in term) for term in annot] for gene in var_names]
+    I = [[int(sequenced_gene in gene_program_gene) for gene_program in gp_dict.keys()] for sequenced_gene in var_names]
     I = np.asarray(I, dtype='int32')
 
     mask = I.sum(0) > min_genes
