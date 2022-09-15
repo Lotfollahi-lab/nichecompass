ecc70f262613c0d31e63f6ead9042247f59ca8d3
sebastianbirk <sebastian.birk@outlook.com>
2022-09-15 21:27:07 +0200

    Updated aggregators

--- autotalker/nn/aggregators.py
+++ autotalker/nn/aggregators.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn import AttentionalAggregation
+import torch.nn.functional as F
+from torch_geometric.nn.inits import glorot
+from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.conv.gcn_conv import gcn_norm
-from torch_geometric.utils import to_dense_adj
+from torch_geometric.utils import to_dense_adj, softmax
 from torch_sparse import SparseTensor
 
 
@@ -15,20 +17,51 @@ class SelfNodeLabelAggregation(nn.Module):
         return node_labels
 
 
-class AttentionNodeLabelAggregation(nn.Module):
-    def __init__(self, n_input: int):
-        super().__init__()
-        self.aggregator = AttentionalAggregation(gate_nn=nn.Linear(n_input, 1))
+class AttentionNodeLabelAggregation(MessagePassing):
+    def __init__(self,
+                 n_input: int,
+                 negative_slope: float=0.2):
+        super().__init__(node_dim=0)
 
-    def forward(self, x, edge_index):
-        n_nodes = x.size(0)
-        adj = to_dense_adj(edge_index=edge_index, max_num_nodes=n_nodes).squeeze(0)
-        print(adj.shape)
-        print(x.shape)
-        x_neighbors_att = self.aggregator(x=x, index=adj)
-        print(x_neighbors_att.shape)
+        self.n_input = n_input
+        self.negative_slope = negative_slope
+
+        self.att = nn.Parameter(torch.Tensor(1, n_input))
+        self._alpha = None
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        glorot(self.att)
+
+    def forward(self, x, edge_index, return_attention_weights: bool=True):
+
+        x_i = x_j = x
+
+        x_neighbors_att = self.propagate(edge_index, x=(x_j, x_i))
         node_labels = torch.cat((x, x_neighbors_att), dim=-1)
-        return node_labels
+
+        alpha = self._alpha
+        self._alpha = None
+
+        if return_attention_weights:
+            assert alpha is not None
+            if isinstance(edge_index, torch.Tensor):
+                return node_labels, (edge_index, alpha)
+            elif isinstance(edge_index, SparseTensor):
+                return node_labels, edge_index.set_value(alpha, layout="coo")
+        else:
+            return node_labels
+
+    def message(self,
+                x_j: torch.Tensor,
+                x_i: torch.Tensor,
+                index: torch.Tensor) -> torch.Tensor:
+        x = x_j + x_i
+        x = F.leaky_relu(x, self.negative_slope)
+        alpha = (x * self.att).sum(dim=-1)
+        alpha = softmax(alpha, index)
+        self._alpha = alpha
+        return x_j * alpha.unsqueeze(-1)
 
 
 class GCNNormNodeLabelAggregation(nn.Module):
