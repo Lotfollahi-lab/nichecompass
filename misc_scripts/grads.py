ec460e11c747df53ccbb93a55ffff160a6394313
sebastianbirk <sebastian.birk@outlook.com>
2022-09-29 09:59:27 +0200

    Add poster and clean up unused files

--- misc_scripts/grads.py
+++
@@ -1,9 +0,0 @@
-        try:
-            grads = []
-            for param in self.encoder.parameters():
-                grads.append(param.grad.view(-1))
-            grads = torch.cat(grads)
-            print("grads", grads.isnan().sum())
-        except:
-            print("no grads")
-
