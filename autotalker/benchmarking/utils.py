e136431609863f85a13fe64c90ed5178a634657d
sebastianbirk <sebastian.birk@outlook.com>
2022-10-12 13:01:14 +0200

    Finalize implementation of graph utils

--- autotalker/benchmarking/utils.py
+++ autotalker/benchmarking/utils.py
@@ -1,4 +1,4 @@
-from typing import Literal, Optional
+from typing import Tuple, Literal, Optional
 
 import numpy as np
 from anndata import AnnData
@@ -8,14 +8,19 @@ from sklearn.neighbors import kneighbors_graph
 from umap.umap_ import nearest_neighbors, fuzzy_simplicial_set
 
 
-def _compute_graph_indices_and_distances(adata: AnnData,
-                                         feature_key: str,
-                                         n_neighbors: int,
-                                         mode: Literal["knn", "umap"]="knn"):
+def _compute_graph_indices_and_distances(
+        adata: AnnData,
+        feature_key: str,
+        n_neighbors: int,
+        mode: Literal["knn", "umap"]="knn",
+        seed: int=42) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Compute a k-nearest-neighbors (knn) graph based on feature values stored in
-    an AnnData object and return the indices of and distances to the k-nearest 
-    neighbors for each observation.
+    Compute indices of and distances to the  ´n_neighbors´ nearest neighbors
+    for each observation by constructing a nearest neighbors graph based on 
+    feature values stored in the  ´obsm´ attribute of an AnnData object.
+    If ´mode´ == "knn", this will be an exact knn graph and if ´mode´ == "umap",
+    this may be exact but is likely approximated via nearest neighbor descent
+    for better performance.
 
     Parameters
     ----------
@@ -26,39 +31,45 @@ def _compute_graph_indices_and_distances(adata: AnnData,
         Key under which the feature values for distance calculation are stored 
         in ´adata.obsm´.
     n_neighbors:
-        Number of neighbors for the knn graph.
+        Number of neighbors for the nearest neighbors graph construction.
     mode:
+        If "knn", compute an exact k-nearest-neighbors (knn) graph using sklearn.
+        If "umap", compute a nearest neighbor graph with nearest neighbor 
+        descent using umap.
+    seed:
+        Random seed to get reproducible results.
 
     Returns
     ----------
     knn_indices:
-        NumPy array that contains the indices of the ´n_neighbors´ nearest 
+        2-D NumPy array that contains the indices of the ´n_neighbors´ nearest 
         neighbors for each observation.
     knn_distances:
-        NumPy array that contains the distances to the ´n_neighbors´ nearest 
+        2-D NumPy array that contains the distances to the ´n_neighbors´ nearest 
         neighbors for each observation.
     """
+    # Get features for graph construction
     X = adata.obsm[feature_key]
+
     if mode == "knn":
-    distances = pairwise_distances(X=X, metric="euclidean")
-
-    # Retrieve knn indices and knn distances from pairwise distances
-    obs_range = np.arange(distances.shape[0])[:, None]
-    knn_indices = np.argpartition(distances, n_neighbors - 1, axis=1)[:, :n_neighbors]
-    knn_indices = knn_indices[obs_range, np.argsort(distances[obs_range,
-                                                    knn_indices])]
-    knn_distances = distances[obs_range, knn_indices]
-
+        # Calculate pairwise feature distances and retrieve knn indices and 
+        # distances
+        distances = pairwise_distances(X=X, metric="euclidean")
+        obs_range = np.arange(distances.shape[0])[:, None]
+        knn_indices = np.argpartition(distances, n_neighbors - 1, axis=1)[:, :n_neighbors]
+        knn_indices = knn_indices[obs_range, np.argsort(distances[obs_range,
+                                                        knn_indices])]
+        knn_distances = distances[obs_range, knn_indices]
     elif mode == "umap":
-    knn_indices, knn_dists, _ = nearest_neighbors(
-        X,
-        n_neighbors,
-        random_state=random_state,
-        metric=metric,
-        metric_kwds=metric_kwds,
-        angular=angular,
-        verbose=verbose,
-    )
+        # Calculate knn indices and distances using nearest neighbors descent
+        knn_indices, knn_distances, _ = nearest_neighbors(
+            X=X,
+            n_neighbors=n_neighbors,
+            random_state=seed,
+            metric="euclidean",
+            metric_kwds=None,
+            angular=None)
+
     return knn_indices, knn_distances
 
 
@@ -69,36 +80,50 @@ def _compute_graph_connectivities(
         mode: Literal["knn", "umap"]="knn",
         seed: int=42) -> coo_matrix:
     """
-    Compute graph connectivities from a feature stored in an AnnData object. The
-    graph can be a simple k-nearest-neighbors graph (´mode´ == "knn") or a 
-    fuzzy simplical set (´mode´ == "umap") as in McInnes, L., Healy, J. & 
-    Melville, J. UMAP: Uniform Manifold Approximation and Projection for 
-    Dimension Reduction. arXiv [stat.ML] (2018).
+    Compute graph connectivities of a nearest neighbors graph based on 
+    feature values stored in the  ´obsm´ attribute of an AnnData object. The
+    connectivites can be computed exactly from a simple k-nearest-neighbors 
+    graph  using sklearn (´mode´ == "knn") or via a fuzzy simplical set based
+    on a neighbor graph approximated via nearest neighbor descent (´mode´ == 
+    "umap") as in McInnes, L., Healy, J. & Melville, J. UMAP: Uniform Manifold 
+    Approximation and Projection for Dimension Reduction. arXiv [stat.ML] (2018).
 
     Parameters
     ----------
     adata:
+        AnnData object with feature values for distance calculation stored in 
+        ´adata.obsm[feature_key]´.
     feature_key:
+        Key under which the feature values for distance calculation are stored 
+        in ´adata.obsm´.
     n_neighbors:
+        Number of neighbors for the nearest neighbors graph construction.
     mode:
+        If "knn", compute exact connectivities from a knn graph using sklearn.
+        If "umap", compute a fuzzy simplical set based on an approximated 
+        neighbor graph.
     seed:
+        Random seed to get reproducible results.
 
     Returns
     ----------
     connectivities:
-
+         Sparse matrix that contains the connectivity weights between all cells.
     """ 
     # Get features for graph construction
     X = adata.obsm[feature_key]
 
-    # Compute graph connectivities
+    # Compute exact graph connectivities
     if mode == "knn":
         connectivities = kneighbors_graph(X=X, n_neighbors=n_neighbors)
+    # Compute graph connectivities using a fuzzy simplical set and approximate
+    # neighbor graph
     elif mode == "umap":
-        knn_indices, knn_distances = _compute_knn_graph_indices_and_distances(
+        knn_indices, knn_distances = _compute_graph_indices_and_distances(
             adata=adata,
             feature_key=feature_key,
-            n_neighbors=n_neighbors)
+            n_neighbors=n_neighbors,
+            mode="umap")
 
         connectivities = fuzzy_simplicial_set(
             X=X,
@@ -149,6 +174,7 @@ def _convert_to_one_hot(vector: np.ndarray,
     """
     if n_classes is None:
         n_classes = np.max(vector) + 1
+
     one_hot = np.zeros(shape=(len(vector), n_classes))
     one_hot[np.arange(len(vector)), vector] = 1
     return one_hot.astype(int)
