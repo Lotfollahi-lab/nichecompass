d62a608b633685014fe9c3f64ea5d4fb06db815e
sebastianbirk <sebastian.birk@outlook.com>
2022-10-13 17:20:29 +0200

    Add graph connectivity distance metric

--- autotalker/benchmarking/__init__.py
+++ autotalker/benchmarking/__init__.py
@@ -1,8 +1,10 @@
 from .cad import compute_avg_cad, compute_cad
+from .gcd import compute_avg_gcd
 from .rclisi import compute_per_cell_log_rclisi
 from .lnmi import compute_max_lnmi
 
 __all__ = ["compute_avg_cad",
            "compute_cad",
+           "compute_avg_gcd",
            "compute_per_cell_log_rclisi",
            "compute_max_lnmi"]
