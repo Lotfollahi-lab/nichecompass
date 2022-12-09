f313fd5d558879f1930399f9ecb253c1232f7ad9
Sebastian Birk <sebastian.birk@outlook.com>
2022-12-09 10:54:33 -0500

    Validate arclisi

--- autotalker/benchmarking/arclisi.py
+++ autotalker/benchmarking/arclisi.py
@@ -1,8 +1,8 @@
 """
 This module contains the Average Absolute Log Relative Cell-Type Local Inverse
-Simpson's Index (ARCLISI) benchmark for testing how good the latent feature
-space preserves neighborhood cell-type heterogeneity from the original spatial
-feature space.
+Simpson's Index (ARCLISI) benchmark for testing how accurately the latent
+feature space preserves neighborhood cell-type heterogeneity from the original
+spatial feature space.
 """
 
 from typing import Optional, Tuple
@@ -23,9 +23,9 @@ def compute_arclisi(
         n_neighbors: int=8,
         seed: int=0) -> float:
     """
-    Compute the average absolute log rclisi (ARCLISI) across all cells. A lower
+    Compute the Average Absolute Log RCLISI (ARCLISI) across all cells. A lower
     value indicates a latent representation that more accurately preserves the
-    spatial cell-type heterogeneity of the ground truth.
+    spatial neighborhood cell-type heterogeneity of the ground truth.
 
     Parameters
     ----------
@@ -51,7 +51,7 @@ def compute_arclisi(
     Returns
     ----------
     arclisi:
-        The average ARCLISI computed over all cells.
+        The Average Absolute Log RCLISI computed over all cells.
     """
     per_cell_log_rclisi_df = compute_per_cell_log_rclisi(
         adata=adata,
@@ -70,44 +70,47 @@ def compute_per_cell_log_rclisi(
         cell_type_key: str="cell-type",
         spatial_key: str="spatial",
         latent_key: str="autotalker_latent",
-        n_neighbors: int=15,
+        n_neighbors: int=8,
         seed: int=0) -> pd.DataFrame:
     """
-    First compute the per-cell Cell-type Local Inverse Simpson's Index (CLISI)
-    from the spatial coordinates (ground truth) and from the latent 
-    representation of the model (latent) respectively. Then divide the latent 
-    CLISI by the ground truth CLISI and compute the log2 to get a relative local
-    cell-type heterogeneity score (RCLISI) as proposed in Heidari, E. et al. 
-    Supervised spatial inference of dissociated single-cell data with SageNet. 
-    bioRxiv 2022.04.14.488419 (2022) doi:10.1101/2022.04.14.488419. 
-    A log RCLISI closer to 0 indicates a latent representation that more 
-    accurately preserves the spatial cell-type heterogeneity of the ground 
+    First, compute the per cell Cell-type Local Inverse Simpson's Index (CLISI)
+    from the spatial coordinates (ground truth) and from the latent
+    representation of the model respectively. Then divide the latent CLISI by
+    the ground truth CLISI and compute the log2 to get a relative local
+    cell-type heterogeneity score, the log Relative Cell-type Local Inverse
+    Simpson's Index (RCLISI) as proposed in Heidari, E. et al. Supervised
+    spatial inference of dissociated single-cell data with SageNet. bioRxiv
+    2022.04.14.488419 (2022) doi:10.1101/2022.04.14.488419.
+    A log RCLISI closer to 0 indicates a latent representation that more
+    accurately preserves the spatial cell-type heterogeneity of the ground
     truth.
 
     Parameters
     ----------
     adata:
-        AnnData object with cell type annotations stored in 
-        ´adata.obs[cell_type_key]´, spatial coordinates stored in 
+        AnnData object with cell type annotations stored in
+        ´adata.obs[cell_type_key]´, spatial coordinates stored in
         ´adata.obsm[spatial_key]´ and the latent representation from the model
-        stored in adata.obsm[latent_rep_key].
+        stored in adata.obsm[latent_key].
     cell_type_key:
         Key under which the cell type annotations are stored in ´adata.obs´.
     spatial_key:
         Key under which the spatial coordinates are stored in ´adata.obsm´.
     latent_key:
-        Key under which the latent representation from the model is stored in 
+        Key under which the latent representation from the model is stored in
         ´adata.obsm´.
     n_neighbors:
-        Number of neighbors used for the construction of the knn graph.
+        Number of neighbors used for the construction of the nearest neighbor
+        graphs from the spatial coordinates and the latent representation from
+        the model.
     seed:
-        Random seed to get reproducible results.
+        Random seed for reproducibility.
 
     Returns
     ----------
     per_cell_log_rclisi_df:
-        Pandas DataFrame that contains the per-cell log RCLISI, the 
-        distribution of which indicates how well the latent space preserves 
+        Pandas DataFrame that contains the per cell log RCLISI, the
+        distribution of which indicates how well the latent space preserves
         ground truth spatial cell-type heterogeneity.
     """
     spatial_per_cell_clisi = _compute_per_cell_clisi_from_feature(
@@ -141,41 +144,41 @@ def _compute_per_cell_clisi_from_feature(
         perplexity: Optional[float]=None,
         seed: int=0) -> np.ndarray:
     """
-    Compute the per-cell Cell-type Local Inverse Simpson's Index (CLISI) by
-    constructing a k-nearest-neighbors (knn) graph based on features stored in 
-    an AnnData object. The per-cell CLISI captures the degree of cell mixing 
-    in a local neighborhood around a given cell. The CLISI score indicates the
-    effective number of different categories represented in the local
-    neighborhood of each cell. If the cells are well-mixed, we might expect the
-    CLISI score to be close to the number of unique cell types (e.g. 
-    neigborhoods with an equal number of cells from 2 cell types get a CLISI of 
-    2). Note, however, that even under perfect mixing, the value would be 
+    Compute the per cell Cell-type Local Inverse Simpson's Index (CLISI) by
+    constructing nearest neighbor graph distances based on
+    ´adata.obsm[feature_key]´. The per cell CLISI captures the degree of cell
+    mixing in a local neighborhood around a given cell. The CLISI score
+    indicates the effective number of different categories represented in the
+    local neighborhood of each cell. If the cells are well mixed, we might
+    expect the CLISI score to be close to the number of unique cell types (e.g.
+    neigborhoods with an equal number of cells from 2 cell types get a CLISI of
+    2). Note, however, that even under perfect mixing, the value would be
     smaller than the number of unique cell types if the absolute number of cells
     is different for different cell types.
 
     Parameters
     ----------
     adata:
-        AnnData object with cell type annotations stored in 
+        AnnData object with cell type annotations stored in
         ´adata.obs[cell_type_key]´ and features for distance calculation stored
         in ´adata.obsm[feature_key]´.
     feature_key:
-        Key under which the features for distance calculation are stored in 
-        ´adata.obsm´.
+        Key under which the feature values for distance calculation are stored
+        in ´adata.obsm´.
     n_neighbors:
-        Number of neighbors used for the construction of the knn graph.
+        Number of neighbors used for the nearest neighbors graph construction.
     cell_type_key:
         Key under which the cell type annotations are stored in ´adata.obs´.
     perplexity:
         Perplexity used for Simpson's Index calculation. By default, perplexity
-        is chosen as ´1/3 * n_neighbors´ used in the knn graph.
+        is chosen as ´1/3 * n_neighbors´.
     seed:
-        Random seed to get reproducible results.
+        Random seed for reproducibility.
 
     Returns
     ----------
     per_cell_clisi:
-        1-D NumPy array that contains the per-cell CLISIs.
+        1-D NumPy array that contains the per cell CLISIs.
     """
     knn_indices, knn_distances = compute_graph_indices_and_distances(
         adata=adata,
@@ -208,17 +211,17 @@ def _compute_per_cell_clisi(
         perplexity: float=15, 
         tolerance: float=1e-5) -> np.ndarray:
     """
-    Compute per-cell Cell-Type Local Inverse Simpson's Index (CLISI) from a 
-    knn graph. Local Inverse Simpson's Index was first proposed in Korsunsky, I.
-    et al. Fast, sensitive and accurate integration of single-cell data with 
-    Harmony. Nat. Methods 16, 1289–1296 (2019). The Inverse Simpson's Index is 
-    the expected number of cells needed to be sampled before two are drawn from 
-    the same category. Thus, this index reports the effective number of 
-    categories in a local neighborhood. LISI combines perplexity-based 
-    neighborhood construction with the Inverse Simpson's Index to account for 
-    distances between neighbors. 
+    Compute per cell Cell-Type Local Inverse Simpson's Index (CLISI) from
+    nearest neighbor graph distances. The LISI was first proposed in Korsunsky,
+    I. et al. Fast, sensitive and accurate integration of single-cell data with
+    Harmony. Nat. Methods 16, 1289–1296 (2019). The Inverse Simpson's Index is
+    the expected number of cells needed to be sampled before two are drawn from
+    the same category. Thus, this index reports the effective number of
+    categories in a local neighborhood. LISI combines perplexity-based
+    neighborhood construction with the Inverse Simpson's Index to account for
+    distances between neighbors.
     
-    Parts of the implementation are adapted from 
+    Parts of the implementation are adapted from
     https://github.com/theislab/scib/blob/29f79d0135f33426481f9ff05dd1ae55c8787142/scib/metrics/lisi.py#L310
     (05.12.22).
 
@@ -226,37 +229,37 @@ def _compute_per_cell_clisi(
     ----------
     knn_distances: 
         2-D NumPy array that contains the distances to the k-nearest-neighbors 
-        for each observation/cell (dimensionality: n_cells x n_neighbors).
+        for each observation / cell (dim: (n_cells, n_neighbors)).
     knn_indices:
         2-D NumPy array that contains the indices of the k-nearest-neighbors for
-        each observation/cell (dimensionality: n_cells x n_neighbors).
+        each observation / cell dim: (n_cells, n_neighbors)).
     cell_type_labels:
-        1-D NumPy array that contains the encoded cell type labels for all 
+        1-D NumPy array that contains the encoded cell type labels for all
         cells.
     n_cell_types:
         Number of unique cell types.
     perplexity:
-        Perplexity used for Simpson's Index calculation (effective neighborhood 
+        Perplexity used for Simpson's Index calculation (effective neighborhood
         size).
     tolerance:
-        Tolerance for the effective neighborhood size of the Gaussian similarity 
+        Tolerance for the effective neighborhood size of the Gaussian similarity
         kernel.
 
     Returns
     ----------
     per_cell_clisi:
-        1-D NumPy array that contains the per-cell CLISIs.
+        1-D NumPy array that contains the per cell CLISIs.
     """
     n_cells = knn_distances.shape[0]
     # Initialize Gaussian similarity kernel neighbor probabilities
     P = np.zeros(knn_distances.shape[1])
-    # Initialize per-cell Cell-Type Local Simpson's Index (CLSI) values
+    # Initialize per cell Cell-Type Local Simpson's Index (CLSI) values
     per_cell_clsi = np.zeros(n_cells)
     log_perplexity = np.log(perplexity)
 
-    # Loop over all cells to compute per-cell CLSI
+    # Loop over all cells to compute per cell CLSI
     for i in range(n_cells):
-        ## 1) Get neighbor probabilities using a Gaussian similarity kernel  
+        ## 1) Get neighbor probabilities using a Gaussian similarity kernel
         beta = 1 # starting precision of Gaussian similarity kernel
         beta_min = -np.inf
         beta_max = np.inf
@@ -264,11 +267,11 @@ def _compute_per_cell_clisi(
         # Get k-nearest-neighbors of current cell
         cell_knn_distances = knn_distances[i, :]
 
-        # Compute the Gaussian similarity kernel neighbor probabilities and 
+        # Compute the Gaussian similarity kernel neighbor probabilities and
         # perplexity for the current precision ´beta´
-        H, P = hbeta(cell_knn_distances, beta)
+        H, P = _hbeta(cell_knn_distances, beta)
 
-        # Evaluate whether the perplexity difference is within tolerance and if 
+        # Evaluate whether the perplexity difference is within tolerance and if
         # not, increase or decrease precision of the Gaussian similarity kernel
         H_diff = H - log_perplexity
         n_tries = 0
@@ -290,7 +293,7 @@ def _compute_per_cell_clisi(
 
             # Recompute the Gaussian similarity kernel neighbor probabilities
             # and perplexity with updated precision ´beta´
-            H, P = hbeta(cell_knn_distances, beta)
+            H, P = _hbeta(cell_knn_distances, beta)
             H_diff = H - log_perplexity
             n_tries += 1
 
@@ -298,14 +301,14 @@ def _compute_per_cell_clisi(
             per_cell_clsi[i] = -1 # default value
             continue
 
-        ## 2) Compute per-cell CLSI values
+        ## 2) Compute per cell CLSI values
         cell_knn_indices = knn_indices[i, :]
         neighbor_cell_type_labels = cell_type_labels[cell_knn_indices]
         neighbor_cell_types_one_hot = convert_to_one_hot(
             neighbor_cell_type_labels, n_cell_types)
-        # Sum P per cell type
+        # Sum P per cell-type
         P_cell_type_sum = np.matmul(P, neighbor_cell_types_one_hot)
-        # Sum square of cell-type specific P_sum's
+        # Sum square of cell-type-specific P_sum's
         per_cell_clsi[i] = np.dot(P_cell_type_sum, P_cell_type_sum)
 
     ## 3) Compute CLISI by inversing CLSI
@@ -313,12 +316,15 @@ def _compute_per_cell_clisi(
     return per_cell_clisi
 
 
-def hbeta(D: np.ndarray, beta: float) -> Tuple[np.ndarray, np.ndarray]:
+def _hbeta(D: np.ndarray, beta: float) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Given a vector of Euclidean distances and the precision of a Gaussian 
-    similarity kernel, compute the normalized Gaussian kernel similarity values 
-    (neighbor probabilities) as well as the entropy. Adapted from
-    https://github.com/theislab/scib/blob/main/scib/metrics/lisi.py#L483.
+    Given a vector of Euclidean distances and the precision of a Gaussian
+    similarity kernel, compute the normalized Gaussian kernel similarity values
+    (neighbor probabilities) as well as the entropy.
+    
+    Implementation is adapted from
+    https://github.com/theislab/scib/blob/main/scib/metrics/lisi.py#L483
+    (01.11.2022).
 
     Parameters
     ----------
@@ -327,6 +333,8 @@ def hbeta(D: np.ndarray, beta: float) -> Tuple[np.ndarray, np.ndarray]:
     beta:
         Precision of the Gaussian similarity kernel.
 
+    Returns
+    ----------
     H:
         Gaussian kernel perplexity.
     P:
