Change neighbor graph computation to scanpy sc.pp.neighbors() function for symmetric neighbor graphs and fix bug in cad benchmarking metric

autotalker/benchmarking/arclisi.py
@@ -9,6 +9,8 @@ from typing import Optional, Tuple
 
 import numpy as np
 import pandas as pd
+import scanpy as sc
+import scipy.sparse as sp
 from anndata import AnnData
 
 from autotalker.utils import compute_graph_indices_and_distances
@@ -180,12 +182,16 @@ def _compute_per_cell_clisi_from_feature(
     per_cell_clisi:
         1-D NumPy array that contains the per cell CLISIs.
     """
-    knn_indices, knn_distances = compute_graph_indices_and_distances(
-        adata=adata,
-        feature_key=feature_key,
+    knn_indices, knn_distances, _ = sc.neighbors.compute_neighbors_umap(
+        X=sp.csr_matrix(adata.X),
         n_neighbors=n_neighbors,
-        mode="knn",
-        seed=seed)
+        random_state=seed)
+    #knn_indices, knn_distances = compute_graph_indices_and_distances(
+    #    adata=adata,
+    #    feature_key=feature_key,
+    #    n_neighbors=n_neighbors,
+    #    mode="knn",
+    #    seed=seed)
 
     if perplexity is None:
         if knn_indices.shape[1] < 3:

autotalker/benchmarking/cad.py
@@ -7,6 +7,7 @@ from the original spatial nearest neighbor graph.
 from typing import Optional
 
 import numpy as np
+import scanpy as sc
 import squidpy as sq
 from anndata import AnnData
 
@@ -154,23 +155,33 @@ def compute_cad(
 
     if spatial_knng_connectivities_key not in adata.obsp:
         # Compute spatial (ground truth) connectivities
-        adata.obsp[spatial_knng_connectivities_key] = (
-            compute_graph_connectivities(
-                adata=adata,
-                feature_key=spatial_key,
-                n_neighbors=n_neighbors,
-                mode="knn",
-                seed=seed))
+        sc.pp.neighbors(adata=adata,
+                        use_rep=spatial_key,
+                        n_neighbors=n_neighbors,
+                        random_state=seed,
+                        key_added=spatial_knng_key)
+        #adata.obsp[spatial_knng_connectivities_key] = (
+        #    compute_graph_connectivities(
+        #        adata=adata,
+        #        feature_key=spatial_key,
+        #        n_neighbors=n_neighbors,
+        #        mode="knn",
+        #        seed=seed))
 
     if latent_knng_connectivities_key not in adata.obsp:
         # Compute latent connectivities
-        adata.obsp[latent_knng_connectivities_key] = (
-            compute_graph_connectivities(
-                adata=adata,
-                feature_key=latent_key,
-                n_neighbors=n_neighbors,
-                mode="knn",
-                seed=seed))
+        sc.pp.neighbors(adata=adata,
+                        use_rep=latent_key,
+                        n_neighbors=n_neighbors,
+                        random_state=seed,
+                        key_added=latent_knng_key)
+        #adata.obsp[latent_knng_connectivities_key] = (
+        #    compute_graph_connectivities(
+        #        adata=adata,
+        #        feature_key=latent_key,
+        #        n_neighbors=n_neighbors,
+        #        mode="knn",
+        #        seed=seed))
 
     # Compute cell-type affinity matrix for spatial nearest neighbor graph
     sq.gr.nhood_enrichment(adata,
@@ -219,6 +230,11 @@ def compute_cad(
     nhood_enrichment_zscores_diff = (
         adata.uns[f"{cell_type_key}_latent_nhood_enrichment"]["zscore"] -
         adata.uns[f"{cell_type_key}_spatial_nhood_enrichment"]["zscore"])
-    cad = np.linalg.norm(nhood_enrichment_zscores_diff,
-                         ord="fro")
+
+    # Remove np.nan ´z_scores´ which can happen as a result of 
+    # ´sq.pl.nhood_enrichment´ permutation
+    nhood_enrichment_zscores_diff = (
+        nhood_enrichment_zscores_diff[~np.isnan(nhood_enrichment_zscores_diff)])
+
+    cad = np.linalg.norm(nhood_enrichment_zscores_diff)
     return cad

autotalker/benchmarking/gcd.py
@@ -7,6 +7,7 @@ original spatial nearest neighbor graph.
 from typing import Optional
 
 import numpy as np
+import scanpy as sc
 from anndata import AnnData
 
 from autotalker.utils import compute_graph_connectivities
@@ -127,23 +128,33 @@ def compute_gcd(
 
     if spatial_knng_connectivities_key not in adata.obsp:
         # Compute spatial (ground truth) connectivities
-        adata.obsp[spatial_knng_connectivities_key] = (
-            compute_graph_connectivities(
-                adata=adata,
-                feature_key=spatial_key,
-                n_neighbors=n_neighbors,
-                mode="knn",
-                seed=seed))
+        sc.pp.neighbors(adata=adata,
+                        use_rep=spatial_key,
+                        n_neighbors=n_neighbors,
+                        random_state=seed,
+                        key_added=spatial_knng_key)
+        #adata.obsp[spatial_knng_connectivities_key] = (
+        #    compute_graph_connectivities(
+        #        adata=adata,
+        #        feature_key=spatial_key,
+        #        n_neighbors=n_neighbors,
+        #        mode="knn",
+        #        seed=seed))
 
     if latent_knng_connectivities_key not in adata.obsp:
         # Compute latent connectivities
-        adata.obsp[latent_knng_connectivities_key] = (
-            compute_graph_connectivities(
-                adata=adata,
-                feature_key=latent_key,
-                n_neighbors=n_neighbors,
-                mode="knn",
-                seed=seed))
+        sc.pp.neighbors(adata=adata,
+                        use_rep=latent_key,
+                        n_neighbors=n_neighbors,
+                        random_state=seed,
+                        key_added=latent_knng_key)
+        #adata.obsp[latent_knng_connectivities_key] = (
+        #    compute_graph_connectivities(
+        #        adata=adata,
+        #        feature_key=latent_key,
+        #        n_neighbors=n_neighbors,
+        #        mode="knn",
+        #        seed=seed))
 
     # Compute Frobenius norm of the matrix of differences to quantify distance
     connectivities_diff = (adata.obsp[latent_knng_connectivities_key] -
@@ -151,4 +162,5 @@ def compute_gcd(
                            ).toarray()
     gcd = np.linalg.norm(connectivities_diff,
                          ord="fro")
+
     return gcd

autotalker/benchmarking/mlnmi.py
@@ -79,23 +79,33 @@ def compute_mlnmi(
 
     if spatial_knng_connectivities_key not in adata.obsp:
         # Compute spatial (ground truth) connectivities
-        adata.obsp[spatial_knng_connectivities_key] = (
-            compute_graph_connectivities(
-                adata=adata,
-                feature_key=spatial_key,
-                n_neighbors=n_neighbors,
-                mode="knn",
-                seed=seed))
+        sc.pp.neighbors(adata=adata,
+                        use_rep=spatial_key,
+                        n_neighbors=n_neighbors,
+                        random_state=seed,
+                        key_added=spatial_knng_key)
+        #adata.obsp[spatial_knng_connectivities_key] = (
+        #    compute_graph_connectivities(
+        #        adata=adata,
+        #        feature_key=spatial_key,
+        #        n_neighbors=n_neighbors,
+        #        mode="knn",
+        #        seed=seed))
 
     if latent_knng_connectivities_key not in adata.obsp:
         # Compute latent connectivities
-        adata.obsp[latent_knng_connectivities_key] = (
-            compute_graph_connectivities(
-                adata=adata,
-                feature_key=latent_key,
-                n_neighbors=n_neighbors,
-                mode="knn",
-                seed=seed))
+        sc.pp.neighbors(adata=adata,
+                        use_rep=latent_key,
+                        n_neighbors=n_neighbors,
+                        random_state=seed,
+                        key_added=latent_knng_key)
+        #adata.obsp[latent_knng_connectivities_key] = (
+        #    compute_graph_connectivities(
+        #        adata=adata,
+        #        feature_key=latent_key,
+        #        n_neighbors=n_neighbors,
+        #        mode="knn",
+        #        seed=seed))
 
     # Define search space of clustering resolutions
     clustering_resolutions = np.linspace(start=0.1,

autotalker/models/autotalker.py
@@ -9,6 +9,7 @@ from typing import Literal, Optional, Tuple, Union
 import mlflow
 import numpy as np
 import pandas as pd
+import scanpy as sc
 import torch
 from anndata import AnnData
 from scipy.special import erfc
@@ -380,7 +381,7 @@ class Autotalker(BaseModelMixin):
         p_h0, the probability that z0 > z1 and p_h1, the probability that 
         z1 >= z0. The rows are ordered by the log Bayes Factor. In addition, the
         (normalized) gene program / latent scores of the 
-        ´n_top_up_gps_retrieved´ top upregulated gene programs and 
+        ´n_top_up_gps_retrieved´ top upregulated gene programs and
         ´n_top_down_gps_retrieved´ top downregulated gene programs will be 
         stored in ´adata.obs´.
 
@@ -763,12 +764,17 @@ class Autotalker(BaseModelMixin):
                               " 'adata.obsm'.")
 
         # Compute latent connectivities
-        adata.obsp["latent_connectivities"] = compute_graph_connectivities(
-            adata=adata,
-            feature_key=self.latent_key_,
-            n_neighbors=n_neighbors,
-            mode=mode,
-            seed=seed)
+        sc.pp.neighbors(adata=adata,
+                        use_rep=self.latent_key_,
+                        n_neighbors=n_neighbors,
+                        random_state=seed,
+                        key_added="latent")
+        #adata.obsp["latent_connectivities"] = compute_graph_connectivities(
+        #    adata=adata,
+        #    feature_key=self.latent_key_,
+        #    n_neighbors=n_neighbors,
+        #    mode=mode,
+        #    seed=seed)
 
     def get_gp_data(self,
                     selected_gps: Optional[Union[str, list]]=None,