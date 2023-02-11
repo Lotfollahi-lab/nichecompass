9f22657d67125eabc2051d57fbcd8bf0db62e055
Sebastian Birk <sebastian.birk@outlook.com>
2023-02-11 13:36:39 -0500

    Fix submodule finding in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 # This is a shim to hopefully allow Github to detect the package, build is done with poetry
 
-import setuptools
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setuptools.setup(
+    setup(
         name="autotalker",
         version='0.1.0',
         description="",
         author="Sebastian Birk",
         author_email="sebastian.birk@outlook.com",
         url="https://github.com/sebastianbirk/autotalker",
-        packages=["autotalker"])
+        packages=find_packages())
