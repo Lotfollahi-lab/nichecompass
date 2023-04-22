Fix latent representation computation after major overhaul

autotalker/models/autotalker.py
@@ -1077,33 +1077,22 @@ class Autotalker(BaseModelMixin):
             n_obs_before_batch = i * node_batch_size
             n_obs_after_batch = n_obs_before_batch + node_batch.batch_size
             node_batch = node_batch.to(device)
-            x = node_batch.x
-            edge_index = node_batch.edge_index
-            conditions = (node_batch.conditions if "conditions" in node_batch
-                          else None)
-            if self.model.log_variational_:
-                x = torch.log(1 + x)
-
             if return_mu_std:
                 mu_batch, std_batch = self.model.get_latent_representation(
-                    x=x,
-                    edge_index=edge_index,
-                    conditions=conditions,
+                    node_batch=node_batch,
                     only_active_gps=only_active_gps,
                     return_mu_std=True)
                 mu[n_obs_before_batch:n_obs_after_batch, :] = (
-                    mu_batch[:node_batch.batch_size].detach().cpu().numpy())
+                    mu_batch.detach().cpu().numpy())
                 std[n_obs_before_batch:n_obs_after_batch, :] = (
-                    std_batch[:node_batch.batch_size].detach().cpu().numpy())
+                    std_batch.detach().cpu().numpy())
             else:
                 z_batch = self.model.get_latent_representation(
-                    x=x,
-                    edge_index=edge_index,
-                    conditions=conditions,
+                    node_batch=node_batch,
                     only_active_gps=only_active_gps,
                     return_mu_std=False)
                 z[n_obs_before_batch:n_obs_after_batch, :] = (
-                    z_batch[:node_batch.batch_size].detach().cpu().numpy())
+                    z_batch.detach().cpu().numpy())
         if return_mu_std:
             return mu, std
         else:
@@ -1317,10 +1306,9 @@ class Autotalker(BaseModelMixin):
             n_obs_before_batch = i * node_batch_size
             n_obs_after_batch = n_obs_before_batch + node_batch.batch_size
 
-            _, alpha = (self.model.gene_expr_node_label_aggregator(
+            _, alpha = (self.model.node_label_aggregator(
                 x=node_batch.x,
                 edge_index=node_batch.edge_index,
-                batch_size=node_batch.batch_size,
                 return_attention_weights=True))
 
             # Get edge index and attention weights of current node batch only
@@ -1389,8 +1377,6 @@ class Autotalker(BaseModelMixin):
         x = dataset.x.to(device)
         edge_index = dataset.edge_index.to(device)
         log_library_size = torch.log(x.sum(1)).unsqueeze(1)
-        if self.model.log_variational_:
-            x = torch.log(1 + x)
 
         mu, _ = self.model.get_latent_representation(
             x=x,

autotalker/modules/vgpgae.py
@@ -679,9 +679,7 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
 
     def get_latent_representation(
             self,
-            x: torch.Tensor,
-            edge_index: torch.Tensor,
-            conditions: Optional[torch.Tensor]=None,
+            node_batch: Data,
             only_active_gps: bool=True,
             return_mu_std: bool=False
             ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
@@ -692,13 +690,8 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
            
         Parameters
         ----------
-        x:
-            Feature matrix to be encoded into latent space (dim: n_obs x 
-            n_genes).
-        edge_index:
-            Edge index of the graph (dim: 2, n_edges).
-        conditions:
-            Conditions for the conditional embedding.
+        node_batch:
+            PyG Data object containing a node-level batch.
         only_active_gps:
             If ´True´, return only the latent representation of active gps.
         return_mu_std:
@@ -714,16 +707,27 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
             Standard deviations of the latent posterior (dim: n_obs, 
             n_active_gps).
         """
+        x = node_batch.x # dim: n_obs x n_omics_features
+        edge_index = node_batch.edge_index # dim: 2 x n_edges
+        # Convert gene expression if done during training
+        if self.log_variational_:
+            x_enc = torch.log(1 + x)
+        else:
+            x_enc = x
+
         # Get conditional embeddings
         if ("encoder" in self.cond_embed_injection_) & (self.n_conditions_ > 0):
-            cond_embed = self.cond_embedder(conditions)
+            cond_embed = self.cond_embedder(
+                node_batch.conditions[:node_batch.batch_size])
         else:
             cond_embed = None
             
         # Get latent distribution parameters
-        mu, logstd = self.encoder(x=x,
-                                  edge_index=edge_index,
-                                  cond_embed=cond_embed)
+        encoder_outputs = self.encoder(x=x_enc,
+                                       edge_index=node_batch.edge_index,
+                                       cond_embed=cond_embed)
+        mu = encoder_outputs[0][:node_batch.batch_size, :]
+        logstd = encoder_outputs[1][:node_batch.batch_size, :]
 
         if only_active_gps:
             # Filter to active gene programs only
@@ -731,6 +735,7 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
             mu, logstd = mu[:, active_gp_mask], logstd[:, active_gp_mask]
 
         if return_mu_std:
+            # (?) check whether this is redundant
             std = torch.exp(logstd)
             return mu, std
         else: