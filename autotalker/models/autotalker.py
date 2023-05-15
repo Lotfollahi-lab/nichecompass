4de3f7813356988acdba20ff36f97c0bfa39769a
Sebastian Birk <sebastian.birk@outlook.com>
2023-05-15 13:32:13 -0400

    Fix another bug in gp summary functionality

--- autotalker/models/autotalker.py
+++ autotalker/models/autotalker.py
@@ -1933,6 +1933,8 @@ class Autotalker(BaseModelMixin):
 
             gp_summary_df["n_source_peaks"] = n_source_peaks
             gp_summary_df["n_target_peaks"] = n_target_peaks
+            gp_summary_df["gp_source_peaks"] = gp_source_peaks
+            gp_summary_df["gp_target_peaks"] = gp_target_peaks
             gp_summary_df["gp_source_peaks_weights_sign_corrected"] = gp_source_peaks_weights
             gp_summary_df["gp_target_peaks_weights_sign_corrected"] = gp_target_peaks_weights
             gp_summary_df["gp_source_peaks_importances"] = gp_source_peaks_importances
