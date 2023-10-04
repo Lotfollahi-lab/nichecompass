8685c3558488bd8403029ac94e001a096f466894
sebastian.birk <sebastian.birk@outlook.com>
2023-10-04 18:48:17 +0200

    Include kbet and pcr in metrics

--- nichecompass/benchmarking/metrics.py
+++ nichecompass/benchmarking/metrics.py
@@ -10,6 +10,8 @@ import time
 from typing import Optional, Union
 
 import mlflow
+import numpy as np
+import scanpy as sc
 import scib_metrics
 from anndata import AnnData
 
@@ -24,21 +26,21 @@ from .nasw import compute_nasw
 
 def compute_benchmarking_metrics(
         adata: AnnData,
-        metrics: list=["gcs", # spatial conservation (unsupervised)
-                       "mlami", # spatial conservation (unsupervised)
-                       "cas", # spatial conservation (cell label supervised)
-                       "clisis", # spatial conservation (cell label supervised)
-                       "cnmi", # biological conservation
-                       "casw", # biological conservation
-                       "clisi", # biological conservation
-                       "nasw", # niche separability
+        metrics: list=["cas", # global spatial conservation (cell label supervised)
+                       "mlami", # global spatial conservation (unsupervised)
+                       "clisis", # local spatial conservation (cell label supervised)
+                       "gcs", # local spatial conservation (unsupervised)
+                       "cnmi", # niche coherence
+                       "nasw", # niche coherence
                        "basw", # batch correction
                        "blisi", # batch correction
-                       "kbet"], # batch correction
+                       "kbet", # batch correction
+                       "pcr"], # batch correction
         cell_type_key: str="cell_type",
         batch_key: Optional[str]=None, 
         spatial_key: str="spatial",
         latent_key: str="nichecompass_latent",
+        pcr_X_pre: Optional[np.array]=None,
         n_jobs: int=1,
         seed: int=0,
         mlflow_experiment_id: Optional[str]=None) -> dict:
@@ -56,6 +58,9 @@ def compute_benchmarking_metrics(
     latent_key:
         Key under which the latent representation from a model is stored in
         ´adata.obsm´.
+    pcr_X_pre:
+        The unintegrated feature space for the computation of the pcr metric.
+        If None, computes PCA on the raw counts stored in ´adata.X´.
     n_jobs:
         Number of jobs to use for parallelization of neighbor search.
     seed:
@@ -85,98 +90,60 @@ def compute_benchmarking_metrics(
     benchmarking_dict = {}
     
     # Compute nearest neighbor graphs
-    if batch_key is None:
-        # Otherwise different metrics require different neighbor graphs and
-        # this will be handled in the metric functions themselves
-        if len(n_neighbors_list) > 0:
-            # Compute spatial nearest neighbor graphs
-            for n_neighbors in n_neighbors_list:
-                if (f"{spatial_key}_{n_neighbors}knng_connectivities"
-                    not in adata.obsp):
-                    print("Computing spatial nearest neighbor graph with "
-                          f"{n_neighbors} neighbors for entire dataset...")
+    # Otherwise different metrics require different neighbor graphs and
+    # this will be handled in the metric functions themselves
+    if len(n_neighbors_list) > 0:
+        # Compute spatial nearest neighbor graphs
+        for n_neighbors in n_neighbors_list:
+            if (f"{spatial_key}_{n_neighbors}knng_connectivities"
+                not in adata.obsp):
+                print("Computing spatial nearest neighbor graph with "
+                      f"{n_neighbors} neighbors for entire dataset...")
+                compute_knn_graph_connectivities_and_distances(
+                        adata=adata,
+                        feature_key=spatial_key,
+                        knng_key=f"{spatial_key}_{n_neighbors}knng",
+                        n_neighbors=n_neighbors,
+                        random_state=seed,
+                        n_jobs=n_jobs)
+            else:
+                print(f"Using precomputed spatial nearest neighbor graph "
+                      f"with {n_neighbors} neighbors...")
+
+        # Compute latent nearest neighbor graphs
+        for n_neighbors in n_neighbors_list:
+            if (f"{latent_key}_{n_neighbors}knng_connectivities"
+                not in adata.obsp):
+                print("Computing latent nearest neighbor graph with "
+                      f"{n_neighbors} neighbors for entire dataset...")
+                try:
                     compute_knn_graph_connectivities_and_distances(
                             adata=adata,
-                            feature_key=spatial_key,
-                            knng_key=f"{spatial_key}_{n_neighbors}knng",
+                            feature_key=latent_key,
+                            knng_key=f"{latent_key}_{n_neighbors}knng",
                             n_neighbors=n_neighbors,
                             random_state=seed,
                             n_jobs=n_jobs)
-                else:
-                    print(f"Using precomputed spatial nearest neighbor graph "
-                          f"with {n_neighbors} neighbors...")
+                except ValueError:
+                    print("Could not compute latent nearest neighbor graph "
+                          f"with {n_neighbors} neighbors for entire "
+                          "dataset...")
+                    continue
+            else:
+                print(f"Using precomputed latent nearest neighbor graph "
+                      f"with {n_neighbors} neighbors...")
 
-            # Compute latent nearest neighbor graphs
-            for n_neighbors in n_neighbors_list:
-                if (f"{latent_key}_{n_neighbors}knng_connectivities"
-                    not in adata.obsp):
-                    print("Computing latent nearest neighbor graph with "
-                          f"{n_neighbors} neighbors for entire dataset...")
-                    try:
-                        compute_knn_graph_connectivities_and_distances(
-                                adata=adata,
-                                feature_key=latent_key,
-                                knng_key=f"{latent_key}_{n_neighbors}knng",
-                                n_neighbors=n_neighbors,
-                                random_state=seed,
-                                n_jobs=n_jobs)
-                    except ValueError:
-                        print("Could not compute latent nearest neighbor graph "
-                              f"with {n_neighbors} neighbors for entire "
-                              "dataset...")
-                        continue
-                else:
-                    print(f"Using precomputed latent nearest neighbor graph "
-                          f"with {n_neighbors} neighbors...")
-                    
-            elapsed_time = time.time() - start_time
-            minutes = int(elapsed_time // 60)
-            seconds = int(elapsed_time % 60)
-            print("Neighbor graphs computed. "
-                  f"Elapsed time: {minutes} minutes "
-                  f"{seconds} seconds.\n")
+        elapsed_time = time.time() - start_time
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        print("Neighbor graphs computed. "
+              f"Elapsed time: {minutes} minutes "
+              f"{seconds} seconds.\n")
             
     # Compute benchmarking metrics
     print("Computing benchmarking metrics...")
     
-    # Spatial conservation metrics (unsupervised)
-    if "gcs" in metrics:
-        print("Computing GCS metric...")
-        benchmarking_dict["gcs"] = compute_gcs(
-            adata=adata,
-            batch_key=batch_key,
-            spatial_knng_key=f"{spatial_key}_15knng",
-            latent_knng_key=f"{latent_key}_15knng",
-            spatial_key=spatial_key,
-            latent_key=latent_key,
-            seed=seed)
-
-        elapsed_time = time.time() - start_time
-        minutes = int(elapsed_time // 60)
-        seconds = int(elapsed_time % 60)
-        print("GCS metric computed. "
-              f"Elapsed time: {minutes} minutes "
-              f"{seconds} seconds.\n")
-        
-    if "mlami" in metrics:
-        print("Computing MLAMI Metric...")
-        benchmarking_dict["mlami"] = compute_mlami(
-            adata=adata,
-            batch_key=batch_key,
-            spatial_knng_key=f"{spatial_key}_15knng",
-            latent_knng_key=f"{latent_key}_15knng",
-            spatial_key=spatial_key,
-            latent_key=latent_key,
-            seed=seed)
-        
-        elapsed_time = time.time() - start_time
-        minutes = int(elapsed_time // 60)
-        seconds = int(elapsed_time % 60)
-        print("MLAMI metric computed. "
-              f"Elapsed time: {minutes} minutes "
-              f"{seconds} seconds.\n")
-        
-    # Spatial conservation metrics (cell-type-based)    
+    # Global spatial conservation metric (cell label supervised)    
     if "cas" in metrics:
         print("Computing CAS metric...")
         benchmarking_dict["cas"] = compute_cas(
@@ -195,7 +162,27 @@ def compute_benchmarking_metrics(
         print("CAS metric computed. "
               f"Elapsed time: {minutes} minutes "
               f"{seconds} seconds.\n")
-              
+
+    # Global spatial conservation metric (unsupervised)
+    if "mlami" in metrics:
+        print("Computing MLAMI Metric...")
+        benchmarking_dict["mlami"] = compute_mlami(
+            adata=adata,
+            batch_key=batch_key,
+            spatial_knng_key=f"{spatial_key}_15knng",
+            latent_knng_key=f"{latent_key}_15knng",
+            spatial_key=spatial_key,
+            latent_key=latent_key,
+            seed=seed)
+        
+        elapsed_time = time.time() - start_time
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        print("MLAMI metric computed. "
+              f"Elapsed time: {minutes} minutes "
+              f"{seconds} seconds.\n")
+    
+    # Local spatial conservation metric (cell label supervised)
     if "clisis" in metrics:
         try:
             print("Computing CLISIS metric...")
@@ -218,9 +205,27 @@ def compute_benchmarking_metrics(
         except:
             print("Could not compute CLISIS metric.")
             benchmarking_dict["clisis"] = 0.
-            
     
-    # Biological conservation metrics
+    # Local spatial conservation metric (unsupervised) 
+    if "gcs" in metrics:
+        print("Computing GCS metric...")
+        benchmarking_dict["gcs"] = compute_gcs(
+            adata=adata,
+            batch_key=batch_key,
+            spatial_knng_key=f"{spatial_key}_15knng",
+            latent_knng_key=f"{latent_key}_15knng",
+            spatial_key=spatial_key,
+            latent_key=latent_key,
+            seed=seed)
+
+        elapsed_time = time.time() - start_time
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        print("GCS metric computed. "
+              f"Elapsed time: {minutes} minutes "
+              f"{seconds} seconds.\n")            
+    
+    # Niche coherence metrics
     if "cnmi" in metrics or "cari" in metrics:
         print("Computing CNMI and CARI metrics...")
         cnmi_cari_dict = scib_metrics.nmi_ari_cluster_labels_kmeans(
@@ -238,7 +243,7 @@ def compute_benchmarking_metrics(
               benchmarking_dict["cnmi"] = cnmi_cari_dict["nmi"]
         if "cari" in metrics:
               benchmarking_dict["cari"] = cnmi_cari_dict["ari"]
-              
+    
     if "casw" in metrics:
         print("Computing CASW Metric...")
         benchmarking_dict["casw"] = scib_metrics.silhouette_label(
@@ -251,7 +256,7 @@ def compute_benchmarking_metrics(
         print("CASW metric computed. "
               f"Elapsed time: {minutes} minutes "
               f"{seconds} seconds.\n")
-              
+    
     if "clisi" in metrics:
         try:
             print("Computing CLISI metric...")
@@ -269,7 +274,6 @@ def compute_benchmarking_metrics(
             print("Could not compute CLISI metric.")
             benchmarking_dict["clisi"] = 0.
         
-    # Niche separability metrics
     if "nasw" in metrics:
         print("Computing NASW Metric...")
         benchmarking_dict["nasw"] = compute_nasw(
@@ -328,6 +332,27 @@ def compute_benchmarking_metrics(
             minutes = int(elapsed_time // 60)
             seconds = int(elapsed_time % 60)
             print("KBET metric computed. "
+                  f"Elapsed time: {minutes} minutes "
+                  f"{seconds} seconds.")
+            
+        if "pcr" in metrics:
+            # https://github.com/yoseflab/scib-metrics/blob/0.4.0/src/scib_metrics/benchmark/_core.py#L171
+            if pcr_X_pre is None:
+                if "X_pca" not in adata.obsm:
+                    sc.tl.pca(adata, use_highly_variable=False)
+                pcr_X_pre = adata.obsm["X_pca"]
+            benchmarking_dict["pcr"] = scib_metrics.pcr_comparison(
+                X_pre=pcr_X_pre,
+                X_post=adata.obsm[latent_key],
+                covariate=adata.obs[batch_key],
+                categorical=True)
+            
+            print(benchmarking_dict["pcr"])
+            
+            elapsed_time = time.time() - start_time
+            minutes = int(elapsed_time // 60)
+            seconds = int(elapsed_time % 60)
+            print("PCR metric computed. "
                   f"Elapsed time: {minutes} minutes "
                   f"{seconds} seconds.")
 
