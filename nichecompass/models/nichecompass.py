Change active gp thresh computation to medians, fix l1 mask, and normalize gp weights with feature scaling factors for active gp determination

nichecompass/models/nichecompass.py
@@ -238,7 +238,6 @@ class NicheCompass(BaseModelMixin):
                  **kwargs):
         self.adata = adata
         self.adata_atac = adata_atac
-
         self.counts_key_ = counts_key
         self.adj_key_ = adj_key
         self.gp_names_key_ = gp_names_key
@@ -310,6 +309,11 @@ class NicheCompass(BaseModelMixin):
                              "reconstruction, you can create a mask of 1s "
                              " that allows all gene program latent nodes to"
                              " reconstruct all genes.")
+            
+        # Determine features scale factors
+        self.features_scale_factors_ = torch.concat(
+            (torch.tensor(self.adata.X.sum(0))[0],
+             torch.tensor(self.adata.X.sum(0))[0]))
     
         # Retrieve chromatin accessibility masks
         if adata_atac is None:
@@ -537,6 +541,7 @@ class NicheCompass(BaseModelMixin):
             target_atac_decoder_mask=self.ca_targets_mask_,
             source_atac_decoder_mask=self.ca_sources_mask_,
             features_idx_dict=self.features_idx_dict_,
+            features_scale_factors=self.features_scale_factors_,
             gene_peaks_mask=gene_peaks_mask,
             cat_covariates_cats=self.cat_covariates_cats_,
             cat_covariates_no_edges=self.cat_covariates_no_edges_,
@@ -720,7 +725,7 @@ class NicheCompass(BaseModelMixin):
                     self.adata.uns[
                         self.targets_categories_label_encoder_key_][category]
                     for category in l1_targets_categories if category in
-                    self.targets_categories_label_encoder_key_]
+                    self.adata.uns[self.targets_categories_label_encoder_key_]]
             if l1_sources_categories is None:
                 l1_sources_categories_encoded = list(self.adata.uns[
                     self.sources_categories_label_encoder_key_].values())
@@ -728,7 +733,8 @@ class NicheCompass(BaseModelMixin):
                 l1_sources_categories_encoded = [
                     self.adata.uns[
                         self.sources_categories_label_encoder_key_][category]
-                    for category in l1_sources_categories]
+                    for category in l1_sources_categories if category in
+                    self.adata.uns[self.sources_categories_label_encoder_key_]]
             l1_targets_mask = torch.from_numpy(np.isin(
                 self.adata.varm[self.gp_targets_categories_mask_key_],
                 l1_targets_categories_encoded))
@@ -1220,6 +1226,8 @@ class NicheCompass(BaseModelMixin):
             Gene program names of active gene programs (dim: n_active_gps,)
         """
         self._check_if_trained(warn=True)
+        
+        device = next(self.model.parameters()).device
 
         active_gp_mask = self.model.get_active_gp_mask()
         active_gp_mask = active_gp_mask.detach().cpu().numpy()
@@ -1344,9 +1352,6 @@ class NicheCompass(BaseModelMixin):
                 self, 
                 adata: Optional[AnnData]=None,
                 adata_atac: Optional[AnnData]=None,
-                counts_key: Optional[str]="counts",
-                adj_key: str="spatial_connectivities",
-                cat_covariates_keys: Optional[List[str]]=None,
                 only_active_gps: bool=True,
                 node_batch_size: int=64,
                 ) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
@@ -1388,9 +1393,9 @@ class NicheCompass(BaseModelMixin):
                 adata=adata,
                 cat_covariates_label_encoders=self.model.cat_covariates_label_encoders_,
                 adata_atac=adata_atac,
-                counts_key=counts_key,
-                adj_key=adj_key,
-                cat_covariates_keys=cat_covariates_keys,
+                counts_key=self.counts_key_,
+                adj_key=self.adj_key_,
+                cat_covariates_keys=self.cat_covariates_keys_,
                 edge_val_ratio=0.,
                 edge_test_ratio=0.,
                 node_val_ratio=0.,
@@ -1658,6 +1663,8 @@ class NicheCompass(BaseModelMixin):
         gp_summary_df:
             DataFrame with gene program summary information.
         """
+        device = next(self.model.parameters()).device
+        
         # Get source and target omics decoder weights
         _, gp_gene_weights, gp_peak_weights = self.get_gp_data()
 

nichecompass/modules/vgpgae.py
@@ -150,6 +150,7 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
                  target_rna_decoder_mask: torch.Tensor,
                  source_rna_decoder_mask: torch.Tensor,
                  features_idx_dict: dict,
+                 features_scale_factors: torch.Tensor,
                  n_output_peaks: int=0,
                  target_atac_decoder_mask: Optional[torch.Tensor]=None,
                  source_atac_decoder_mask: Optional[torch.Tensor]=None,
@@ -213,6 +214,7 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
         self.target_atac_decoder_mask = target_atac_decoder_mask
         self.source_atac_decoder_mask = source_atac_decoder_mask
         self.features_idx_dict_ = features_idx_dict
+        self.features_scale_factors_ = features_scale_factors
         self.gene_peaks_mask_ = gene_peaks_mask
         #assert(torch.all(torch.logical_or(gene_peaks_mask == 0,
         #                                  gene_peaks_mask == 1)))
@@ -1197,8 +1199,10 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
             self,
             abs_gp_weights_agg_mode: Literal["sum",
                                              "nzmeans",
-                                             "sum+nzmeans"]="sum+nzmeans",
-            return_gp_weights: bool=False
+                                             "sum+nzmeans",
+                                             "nzmedians",
+                                             "sum+nzmedians"]="sum+nzmedians",
+            return_gp_weights: bool=False,
             ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
         """
         Get a mask of active gene programs based on the rna decoder gene weights
@@ -1233,9 +1237,11 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
             Tensor containing the rna decoder gene weights of active gene
             programs.
         """
+        device = next(self.parameters()).device
+        
         active_gp_mask = torch.zeros(self.n_prior_gp_ + self.n_addon_gp_,
                                      dtype=torch.bool,
-                                     device=next(self.parameters()).device)
+                                     device=device)
 
         if (self.n_addon_gp_ > 0):
             gp_types = ["masked", "addon"]
@@ -1245,23 +1251,27 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
         for gp_type in gp_types:
             gp_weights = self.get_gp_weights(only_masked_features=False,
                                              gp_type=gp_type)[0]
-
+            
             # Get index of gps based on ´gp_type´
             if gp_type == "masked":
                 gp_idx = slice(None, self.n_prior_gp_)
             elif gp_type == "addon":
                 gp_idx = slice(self.n_prior_gp_, None)
-
+            
+            # Normalize gp weights with feature scales
+            gp_weights_normalized = (gp_weights /
+                                     self.features_scale_factors_[:, None].to(device))
+            
             # Normalize gp weights with running mean absolute gp scores
             gp_weights_normalized = (self.running_mean_abs_mu[gp_idx] *
-                                     gp_weights)
-            
+                                     gp_weights_normalized)
+
             # Aggregate absolute normalized gp weights based on
             # ´abs_gp_weights_agg_mode´ and calculate thresholds of aggregated
             # absolute normalized gp weights and get active gp mask and (optionally)
             # active gp weights
             abs_gp_weights_sums = gp_weights_normalized.norm(p=1, dim=0)
-            if abs_gp_weights_agg_mode in ["sum", "sum+nzmeans"]:
+            if abs_gp_weights_agg_mode in ["sum", "sum+nzmeans", "sum+nzmedians"]:
                 max_abs_gp_weights_sum = max(abs_gp_weights_sums)
                 min_abs_gp_weights_sum_thresh = (self.active_gp_thresh_ratio_ * 
                                                 max_abs_gp_weights_sum)
@@ -1285,6 +1295,23 @@ class VGPGAE(nn.Module, BaseModuleMixin, VGAEModuleMixin):
                     active_gp_mask[gp_idx] = active_gp_mask[gp_idx] | (
                         abs_gp_weights_nzmeans >=
                         min_abs_gp_weights_nzmean_thresh)
+            if abs_gp_weights_agg_mode in ["nzmedians", "sum+nzmedians"]:
+                zero_mask = (gp_weights_normalized == 0)
+                abs_gp_weights_normalized_with_nan = torch.where(zero_mask, torch.tensor(float('nan')), torch.abs(gp_weights_normalized))
+                abs_gp_weights_nzmedians = torch.nanmedian(abs_gp_weights_normalized_with_nan, dim=0).values
+                abs_gp_weights_nzmedians = torch.nan_to_num(abs_gp_weights_nzmedians)
+                max_abs_gp_weights_nzmedian = torch.max(abs_gp_weights_nzmedians)
+                min_abs_gp_weights_nzmedian_thresh = (0.01 *
+                                                      max_abs_gp_weights_nzmedian)
+                if abs_gp_weights_agg_mode == "nzmedians":
+                    active_gp_mask[gp_idx] = active_gp_mask[gp_idx] | (
+                        abs_gp_weights_nzmedians >= 
+                        min_abs_gp_weights_nzmedian_thresh)
+                elif abs_gp_weights_agg_mode == "sum+nzmedians":
+                    # Combine active gp mask
+                    active_gp_mask[gp_idx] = active_gp_mask[gp_idx] | (
+                        abs_gp_weights_nzmedians >=
+                        min_abs_gp_weights_nzmedian_thresh)
         if return_gp_weights:
             active_gp_weights = gp_weights[:, active_gp_mask]
             return active_gp_mask, active_gp_weights