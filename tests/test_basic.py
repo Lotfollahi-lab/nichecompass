3ed4e4f905ebf5da4a1066607320a4589192d894
Sebastian Birk <sebastian.birk@outlook.com>
2024-02-13 09:55:17 +0100

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,7 +6,5 @@ import pytest
 #def test_package_has_version():
 #    assert nichecompass.__version__ is not None
 
-
-#@pytest.mark.skip(reason="This decorator should be removed when test passes.")
-#def test_example():
-#    assert 1 == 0  # This test is designed to fail.
+def test_example():
+    assert 1 == 1  # This test is designed to pass.
