ccb28aede9a596cb70925fc07a50b63d90970dee
sebastianbirk <sebastian.birk@outlook.com>
2024-02-13 10:14:02 +0100

    Readd test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,7 @@
 import pytest
 
-#import nichecompass
+import nichecompass
 
 
-#def test_package_has_version():
-#    assert nichecompass.__version__ is not None
-
-def test_example():
-    assert 1 == 1  # This test is designed to pass.
+def test_package_has_version():
+    assert nichecompass.__version__ is not None
